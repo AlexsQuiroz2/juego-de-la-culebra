8ae07f00ac74fb39b58812f62357b9d5d849c17b
alexs <alexsquiroz054@gmail.com>
2022-11-18 08:21:43 -0500

    version 2

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-from turtle import Screen, Turtle
+from turtle import Screen  # Desde la libreria Turtle importa el screen 
+from snake import Snake    # Llama el archvo snake y importa la clase Snake
+from food import Food     # Llama el archvo food y importa la clase Food
+from scoreboard import Scoreboard   # Llama el archvo scoreboard y importa la clase Scoreboard
 import time
 
-screen = Screen()
-
-#creacion de la ventana
-screen.setup(width=645, height=700)
+screen = Screen()   # Llama un screen
+screen.setup(width=600, height=600) # Crea los limites de la ventana
 screen.bgcolor("white")
-screen.title("Snake game")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-#movimiento
-segments = []
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-#posision de la culebra
-starting_position = [(0,0),(25,0),(50,0)]
-
-for position in starting_position:
-    new_segment = Turtle("turtle")
-    new_segment.color("lawngreen")
-    new_segment.penup()
-    new_segment.goto(position)
-    #movimiento
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
-    time.sleep(.1)
-    for seg in segments:
-        seg.forward(25)
-        seg.left(35) 
+    time.sleep(0.1)
+    snake.move()
 
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 300 or snake.head.xcor() < -319 or snake.head.ycor() > 310 or snake.head.ycor() < -310:
+        game_is_on = False
+        scoreboard.game_over()
+
+#Detect collision with tail.
+for segment in snake.segments:
+    if segment == snake.head:
+        pass
+    elif snake.head.distance(segment) < 10:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
--- readme.md
+++ readme.md
@@ -1,13 +1,17 @@
-To do
-<br>
-- crear la ventana
-- Crear el cuerpo de la culbra
-- Mover la culebra
-- Controlar movimiento
-- Detectar colision con la comida
-- Manejar puntaje
-- Detectar colision con los muros
-- Detectar colision con el mismo cuerpo
+<h1>Main</h1>
 
 
-- pagina del modulo turtle: https://www.mclibre.org/consultar/python/lecciones/python-turtle-1.html
+
+
+<h1>Food</h1>
+
+
+
+<h1>Scoreboard</h1>
+
+
+
+<h1>Snake</h1>
+
+
+
